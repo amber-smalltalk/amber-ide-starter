f48cc2dceab4956f4e86fd019b22507fb386ca3b
Herbert Vojčík <herby@mailbox.sk>
2014-10-08 09:12:51 +0200

    Initial commit

---
+++ config.js
@@ -0,0 +1,11 @@
+/* DO NOT EDIT! This file is generated. */
+
+var require;
+if (!require) require = {config: function (x) {require = x;}};
+require.config({
+  "paths": {
+    "mousetrap": "bower_components/mousetrap/mousetrap",
+    "jquery.blockUI": "bower_components/blockui/jquery.blockUI",
+    "amber-ide-starter-dialog": "lib/idestarter"
+  }
+});
